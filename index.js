1a33e0b4e01618d7432b3a6b9728f7a4ef4d2bf3
Tao <werewaswas@live.com>
2018-08-03 22:14:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,9 +57,9 @@ class Trip {
   }
 
   driver() {
-    return store.drivers.find(driver => {
+    return store.drivers.find(function (driver) {
       return driver.id === this.driverId
-    })
+    }.bind(this))
   }
 
 //   driver() {
