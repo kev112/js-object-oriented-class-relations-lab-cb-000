9e2c043e4605143250b2fe4f57a95e05535ef1a2
Tao <werewaswas@live.com>
2018-08-03 22:18:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,14 +56,15 @@ class Trip {
     store.trips.push(this)
   }
 
-  // driver() {
-  //   return store.drivers.find(function (driver) {
-  //     return driver.id === this.driverId
-  //   }.bind(this))
-  // }
-  // 
-  // // passenger() {
-  // //   return store.passengers.find(function (passenger) {
-  // //     return passenger.id == this.passengerId
-  // //   }.bind(this))
-  // // }
+  driver() {
+    return store.drivers.find(function (driver) {
+      return driver.id === this.driverId
+    }.bind(this))
+  }
+
+  passenger() {
+    return store.passengers.find(function (passenger) {
+      return passenger.id == this.passengerId
+    }.bind(this))
+  }
+}
