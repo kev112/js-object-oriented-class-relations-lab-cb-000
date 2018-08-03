fe74629fc4e61f94d28c8fd97c24972a950b8c36
Tao <werewaswas@live.com>
2018-08-03 21:41:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-Driver class:
-
-A driver has many trips, and has many passengers through trips.
-new Driver() - initialized with a name; returns a JavaScript object that has attributes of id, and name
-trips() - returns all of the trips that a driver has taken
-passengers() - returns all of the passengers that a driver has taken on a trip
-
 let driverId = 0
 let tripId = 0
 let passengerId = 0
